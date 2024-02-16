f25698eb6c3fdcfc38d5b26a5a82c78aaa3362b2
Dini <dini@Dinis-MacBook-Air.local>
2024-02-16 18:40:19 +0800

    added app/api/graphql

---
+++ app/api/graphql/route.ts
@@ -0,0 +1,18 @@
+import { ApolloServer } from "@apollo/server";
+import { startServerAndCreateNextHandler } from "@as-integrations/next";
+import { PrismaClient } from "@prisma/client";
+import { prisma } from "@/prisma/db";
+import { typeDefs } from "@/graphql/schema";
+import { resolvers } from "@/graphql/resolvers";
+
+export type Context = {
+  prisma: PrismaClient;
+};
+
+const apolloServer = new ApolloServer<Context>({ typeDefs, resolvers });
+
+const handler = startServerAndCreateNextHandler(apolloServer, {
+  context: async (req, res) => ({ req, res, prisma })
+});
+
+export { handler as GET, handler as POST };
