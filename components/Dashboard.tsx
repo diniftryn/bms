6c0ed69334871e1d87cc36a259d37b51bfdba8c0
Dini <dini@Dinis-MacBook-Air.local>
2024-02-17 01:13:46 +0800

    added search and avatar to dashboard layout

--- components/Dashboard.tsx
+++ components/Dashboard.tsx
@@ -1,4 +1,3 @@
-import { Metadata } from "next";
 import Image from "next/image";
 import { PlusCircledIcon } from "@radix-ui/react-icons";
 
@@ -7,6 +6,10 @@ import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
 
 import { cn } from "@/lib/utils";
 import Link from "next/link";
+import { Input } from "./ui/input";
+
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuGroup, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuShortcut, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
 
 export default function DashboardLayout({ children }: { children: React.ReactNode }) {
   return (
@@ -126,19 +129,61 @@ function Sidebar({ className }: { className: string }) {
 
 function MainNav({ className, ...props }: React.HTMLAttributes<HTMLElement>) {
   return (
-    <nav className={cn("flex items-center space-x-4 lg:space-x-6", className)} {...props}>
-      <Link href="/examples/dashboard" className="text-sm font-medium transition-colors hover:text-primary">
-        Overview
-      </Link>
-      <Link href="/examples/dashboard" className="text-sm font-medium text-muted-foreground transition-colors hover:text-primary">
-        Customers
-      </Link>
-      <Link href="/examples/dashboard" className="text-sm font-medium text-muted-foreground transition-colors hover:text-primary">
-        Products
-      </Link>
-      <Link href="/examples/dashboard" className="text-sm font-medium text-muted-foreground transition-colors hover:text-primary">
-        Settings
-      </Link>
-    </nav>
+    <div className="pr-5 flex items-center justify-end space-x-4 h-16">
+      <Search />
+      <UserNav />
+    </div>
+  );
+}
+
+function Search() {
+  return (
+    <div>
+      <Input type="search" placeholder="Search..." className="md:w-[100px] lg:w-[300px]" />
+    </div>
+  );
+}
+
+function UserNav() {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="ghost" className="relative h-8 w-8 rounded-full">
+          <Avatar className="h-8 w-8">
+            <AvatarImage src="/avatars/01.png" alt="@shadcn" />
+            <AvatarFallback>SC</AvatarFallback>
+          </Avatar>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="w-56" align="end" forceMount>
+        <DropdownMenuLabel className="font-normal">
+          <div className="flex flex-col space-y-1">
+            <p className="text-sm font-medium leading-none">shadcn</p>
+            <p className="text-xs leading-none text-muted-foreground">m@example.com</p>
+          </div>
+        </DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuGroup>
+          <DropdownMenuItem>
+            Profile
+            <DropdownMenuShortcut>⇧⌘P</DropdownMenuShortcut>
+          </DropdownMenuItem>
+          <DropdownMenuItem>
+            Billing
+            <DropdownMenuShortcut>⌘B</DropdownMenuShortcut>
+          </DropdownMenuItem>
+          <DropdownMenuItem>
+            Settings
+            <DropdownMenuShortcut>⌘S</DropdownMenuShortcut>
+          </DropdownMenuItem>
+          <DropdownMenuItem>New Team</DropdownMenuItem>
+        </DropdownMenuGroup>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem>
+          Log out
+          <DropdownMenuShortcut>⇧⌘Q</DropdownMenuShortcut>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
   );
 }
