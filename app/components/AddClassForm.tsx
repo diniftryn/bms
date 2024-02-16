e363fb2aadccdef891db83199310ec36d7056d21
Dini <dini@Dinis-MacBook-Air.local>
2024-02-16 20:54:29 +0800

    styled add new class form

--- app/components/AddClassForm.tsx
+++ app/components/AddClassForm.tsx
@@ -3,6 +3,7 @@ import { ADD_CLASS } from "@/graphql/mutations/classMutations";
 import { GET_CLASSES } from "@/graphql/queries/classQueries";
 import { GET_INSTRUCTORS } from "@/graphql/queries/instructorQueries";
 import { useMutation, useQuery } from "@apollo/client";
+import { Button, Input, Select, Textarea } from "@chakra-ui/react";
 import { useRouter } from "next/navigation";
 import React, { FormEvent, useState } from "react";
 
@@ -39,36 +40,38 @@ export default function AddClassForm() {
   if (error) return "Something Went Wrong";
 
   return (
-    <div>
+    <div className="grid justify-center items-center p-5">
+      <p className="text-2xl font-bold pb-5">Add a New Class</p>
       {!loading && !error && (
-        <form onSubmit={handleSubmit}>
-          <div className="grid grid-cols-2 gap-2">
+        <form onSubmit={handleSubmit} className="lg:max-w-[50vw]">
+          <div className="grid grid-cols-3 gap-2">
             <label>Name</label>
-            <input className="border" type="text" id="name" value={name} onChange={e => setName(e.target.value)} />
+            <Input className="col-span-2" type="text" id="name" value={name} onChange={e => setName(e.target.value)} />
 
             <label>Description</label>
-            <textarea className="border" id="description" value={description} onChange={e => setDescription(e.target.value)}></textarea>
+            <Textarea className="col-span-2" id="description" value={description} onChange={e => setDescription(e.target.value)} />
 
             <label>Status</label>
-            <select className="border" id="status" value={status} onChange={e => setStatus(e.target.value)}>
+            <Select className="col-span-2" id="status" value={status} onChange={e => setStatus(e.target.value)}>
               <option value="new">Coming Soon</option>
               <option value="enrolment">Enrolment</option>
               <option value="progress">In Progress</option>
               <option value="completed">Completed</option>
-            </select>
+            </Select>
 
-            <label>Instructor</label>
-            <select className="border" id="instructorId" value={instructorId} onChange={e => setInstructorId(e.target.value)}>
-              <option value="">Select Instructor</option>
+            <label className="col-start-1">Instructor</label>
+            <Select id="instructorId" placeholder="Select Instructor" value={instructorId} onChange={e => setInstructorId(e.target.value)}>
               {data.instructors.map((instructor: { id: string; name: string }) => (
                 <option key={instructor.id} value={instructor.id}>
                   {instructor.name}
                 </option>
               ))}
-            </select>
+            </Select>
           </div>
 
-          <button type="submit">Submit</button>
+          <Button colorScheme="blue" type="submit">
+            Submit
+          </Button>
         </form>
       )}
     </div>
