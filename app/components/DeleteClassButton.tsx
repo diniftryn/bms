5b88e79ce6953765ef08e25098426ad4db955e8c
Dini <dini@Dinis-MacBook-Air.local>
2024-02-16 19:49:03 +0800

    designed styled delete class button

--- app/components/DeleteClassButton.tsx
+++ app/components/DeleteClassButton.tsx
@@ -4,12 +4,13 @@ import { GET_CLASSES } from "@/graphql/queries/classQueries";
 import { useMutation } from "@apollo/client";
 import { MouseEventHandler } from "react";
 
-// import { AlertDialog, AlertDialogBody, AlertDialogFooter, AlertDialogHeader, AlertDialogContent, AlertDialogOverlay, Button, useDisclosure, useToast, AlertIcon } from "@chakra-ui/react";
+import { AlertDialog, AlertDialogBody, AlertDialogFooter, AlertDialogHeader, AlertDialogContent, AlertDialogOverlay, Button, useDisclosure, useToast, AlertIcon } from "@chakra-ui/react";
+import React from "react";
 
 export default function DeleteClassButton({ classId }: { classId: string }) {
-  // const { isOpen, onOpen, onClose } = useDisclosure();
-  // const cancelRef = React.useRef();
-  // const toast = useToast();
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const cancelRef = React.useRef();
+  const toast = useToast();
 
   const [deleteClass] = useMutation(DELETE_CLASS, {
     variables: { id: classId },
@@ -19,48 +20,60 @@ export default function DeleteClassButton({ classId }: { classId: string }) {
   const handleDelete: MouseEventHandler<HTMLButtonElement> = e => {
     e.preventDefault();
 
-    deleteClass({
+    const promise = deleteClass({
       variables: {
         id: classId
       }
     });
 
-    // toast({
-    //   title: "Successful",
-    //   description: "Class successfully deleted.",
-    //   status: "success",
-    //   duration: 2000,
-    //   isClosable: true
-    // });
+    toast.promise(promise, {
+      success: {
+        title: "Successful",
+        description: "Class successfully deleted.",
+        duration: 2000,
+        isClosable: true
+      },
+      error: {
+        title: "Error",
+        description: "Could not delete class.",
+        duration: 2000,
+        isClosable: true
+      },
+      loading: {
+        title: "Deleting class...",
+        description: "Please do not close the page.",
+        duration: 2000,
+        isClosable: true
+      }
+    });
   };
 
   return (
-    <button onClick={handleDelete}>Delete Class</button>
-    // <>
-    //   <Button colorScheme="red" onClick={onOpen}>
-    //     Delete Class
-    //   </Button>
+    <>
+      <Button colorScheme="red" onClick={onOpen}>
+        Delete Class
+      </Button>
 
-    //   <AlertDialog isOpen={isOpen} leastDestructiveRef={cancelRef} onClose={onClose}>
-    //     <AlertDialogOverlay>
-    //       <AlertDialogContent>
-    //         <AlertDialogHeader fontSize="lg" fontWeight="bold">
-    //           Delete Class
-    //         </AlertDialogHeader>
+      <AlertDialog isOpen={isOpen} leastDestructiveRef={cancelRef} onClose={onClose}>
+        <AlertDialogOverlay>
+          <AlertDialogContent>
+            <AlertDialogHeader fontSize="lg" fontWeight="bold">
+              Delete Class
+            </AlertDialogHeader>
 
-    //         <AlertDialogBody>Are you sure? You can't undo this action afterwards.</AlertDialogBody>
+            <AlertDialogBody>Are you sure? You can't undo this action afterwards.</AlertDialogBody>
 
-    //         <AlertDialogFooter>
-    //           <Button ref={cancelRef} onClick={onClose}>
-    //             Cancel
-    //           </Button>
-    //           <Button colorScheme="red" onClick={handleDelete} ml={3}>
-    //             Confirm Delete
-    //           </Button>
-    //         </AlertDialogFooter>
-    //       </AlertDialogContent>
-    //     </AlertDialogOverlay>
-    //   </AlertDialog>
-    // </>
+            <AlertDialogFooter>
+              <Button ref={cancelRef} onClick={onClose}>
+                Cancel
+              </Button>
+              <Button colorScheme="red" onClick={handleDelete} ml={3}>
+                Confirm Delete
+              </Button>
+            </AlertDialogFooter>
+          </AlertDialogContent>
+        </AlertDialogOverlay>
+      </AlertDialog>
+    </>
   );
 }
--- app/components/Providers.tsx
+++ app/components/Providers.tsx
@@ -1,11 +1,16 @@
 "use client";
 import React from "react";
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import { ChakraProvider } from "@chakra-ui/react";
 
 export const Providers = ({ children }: { children: React.ReactNode }) => {
   const client = new ApolloClient({
     uri: process.env.NEXT_PUBLIC_URL + "/api/graphql",
     cache: new InMemoryCache()
   });
-  return <ApolloProvider client={client}>{children}</ApolloProvider>;
+  return (
+    <ChakraProvider>
+      <ApolloProvider client={client}>{children}</ApolloProvider>
+    </ChakraProvider>
+  );
 };
