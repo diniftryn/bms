ca23b8d7a3002a6f244c7cd7accd4ed08c81815e
Dini <dini@Dinis-MacBook-Air.local>
2024-02-16 17:36:54 +0800

    modified AddInstructorForm

--- app/components/AddInstructorForm.tsx
+++ app/components/AddInstructorForm.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { ADD_INSTRUCTOR } from "@/graphql/mutations/instructorMutations";
 import { GET_INSTRUCTORS } from "@/graphql/queries/instructorQueries";
-import { useMutation, useQuery } from "@apollo/client";
+import { useMutation } from "@apollo/client";
 import { useRouter } from "next/navigation";
 import React, { FormEvent, useState } from "react";
 
@@ -31,28 +31,20 @@ export default function AddInstructorForm() {
     });
   };
 
-  const { loading, error, data } = useQuery(GET_INSTRUCTORS);
-  if (loading) return null;
-  if (error) return "Something Went Wrong";
-
   return (
-    <div>
-      {!loading && !error && (
-        <form onSubmit={handleSubmit}>
-          <div className="grid grid-cols-2 gap-2">
-            <label>Name</label>
-            <input className="border" type="text" id="name" value={name} onChange={e => setName(e.target.value)} />
+    <form onSubmit={handleSubmit}>
+      <div className="grid grid-cols-2 gap-2">
+        <label>Name</label>
+        <input className="border" type="text" id="name" value={name} onChange={e => setName(e.target.value)} />
 
-            <label>Phone</label>
-            <input className="border" type="text" id="phone" value={phone} onChange={e => setPhone(e.target.value)} />
+        <label>Phone</label>
+        <input className="border" type="text" id="phone" value={phone} onChange={e => setPhone(e.target.value)} />
 
-            <label>Email</label>
-            <input className="border" type="text" id="email" value={email} onChange={e => setEmail(e.target.value)} />
-          </div>
+        <label>Email</label>
+        <input className="border" type="text" id="email" value={email} onChange={e => setEmail(e.target.value)} />
+      </div>
 
-          <button type="submit">Submit</button>
-        </form>
-      )}
-    </div>
+      <button type="submit">Submit</button>
+    </form>
   );
 }
